bd95c39e7b985705247a32baa7581858b4790f96
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2023-09-12 20:38:04 -0700

    Added two retries, check the date matches today, and exit 1 if it doesn't

--- call_api.py
+++ call_api.py
@@ -1,43 +1,37 @@
 import requests
 import sys
 import json
+from datetime import datetime
+import time
 
-# Use of the monitoring server API is subject to a query limit of 300 requests for a specific account token per day
-# The monitoring server API allows up to 3 concurrent API calls from the same source IP.
+RETRIES = 2
+DELAY = 60  # seconds
 
 BASE_URL = "https://monitoringapi.solaredge.com/site/{}/{}"
+SITES_URL = "https://monitoringapi.solaredge.com/sites/list"
 
 
-def fetch_detail_data_from_solaredge(site_id, api_key):
-    endpoint = BASE_URL.format(site_id, "details")
-    print("Calling endpoint: ", endpoint)
-    response = requests.get(endpoint, params={"api_key": api_key})
+def fetch_data_from_solaredge(url, api_key):
+    print("Calling endpoint: ", url)
+    response = requests.get(url, params={"api_key": api_key})
     response.raise_for_status()
     return response.json()
 
 
-def fetch_sites_data_from_solaredge(api_key):
-    endpoint = "https://monitoringapi.solaredge.com//sites/list"
-    print("Calling endpoint: ", endpoint)
-    response = requests.get(endpoint, params={"api_key": api_key})
-    response.raise_for_status()
-    return response.json()
+def check_data_period_end_date(data_period):
+    # Extract the endDate from the dataPeriod
+    end_date_str = data_period.get('dataPeriod', {}).get('endDate', '')
 
+    # Get today's date in the same format
+    today_str = datetime.today().strftime('%Y-%m-%d')
 
-def fetch_overview_data_from_solaredge(site_id, api_key):
-    endpoint = BASE_URL.format(site_id, "overview")
-    print("Calling endpoint: ", endpoint)
-    response = requests.get(endpoint, params={"api_key": api_key})
-    response.raise_for_status()
-    return response.json()
-
-
-def fetch_alert_data_from_solaredge(site_id, api_key):
-    endpoint = BASE_URL.format(site_id, "alerts")
-    print("Calling endpoint: ", endpoint)
-    response = requests.get(endpoint, params={"api_key": api_key})
-    response.raise_for_status()
-    return response.json()
+    # Compare and return the result
+    if end_date_str == today_str:
+        return 0
+    else:
+        error_message = f"Error: The endDate {end_date_str} does not match today's date {today_str}."
+        print(error_message)
+        return 1
 
 
 def main():
@@ -48,34 +42,33 @@ def main():
         site_id = input("Please enter your SolarEdge Site ID: ").strip()
         api_key = input("Please enter your SolarEdge API key: ").strip()
 
-    try:
-        data = fetch_detail_data_from_solaredge(site_id, api_key)
-        print("Site Details:\n", json.dumps(data, indent=4))
-    except requests.HTTPError as e:
-        print(e.response.text)
-        sys.exit(1)
+    endpoints = {
+        # "Site Details": BASE_URL.format(site_id, "details"),
+        # "Sites List": SITES_URL,
+        # "Overview": BASE_URL.format(site_id, "overview"),
+        "Data Period": BASE_URL.format(site_id, "dataPeriod"),
+        # "Alerts": BASE_URL.format(site_id, "alerts")
+    }
 
-    try:
-        data = fetch_sites_data_from_solaredge(api_key)
-        print("Sites List:\n", json.dumps(data, indent=4))
-    except requests.HTTPError as e:
-        print(e.response.text)
-        sys.exit(1)
+    for description, url in endpoints.items():
+        retries_left = RETRIES
+        while retries_left > 0:
+            try:
+                data = fetch_data_from_solaredge(url, api_key)
+                print(f"{description}:\n", json.dumps(data, indent=4))
+                break  # If successful, break out of the retry loop
+            except requests.HTTPError as e:
+                print(f"Error fetching {description}. Retries left: {retries_left}")
+                print(e.response.text)
+                retries_left -= 1
+                if retries_left > 0:  # Only sleep if there are retries left
+                    time.sleep(DELAY)
+        else:  # This will execute if the while loop exhausted all retries without a break
+            print(f"Failed to fetch {description} after {RETRIES} retries.")
+            sys.exit(1)
 
-    try:
-        data = fetch_overview_data_from_solaredge(site_id, api_key)
-        print("Overview:\n", json.dumps(data, indent=4))
-    except requests.HTTPError as e:
-        print(e.response.text)
-        sys.exit(1)
-
-
-    try:
-        data = fetch_alert_data_from_solaredge(site_id, api_key)
-        print("Alerts:\n", json.dumps(data, indent=4))
-    except requests.HTTPError as e:
-        print(e.response.text)
-        sys.exit(1)
+        result = check_data_period_end_date(data)
+        sys.exit(result)
 
 
 if __name__ == "__main__":
