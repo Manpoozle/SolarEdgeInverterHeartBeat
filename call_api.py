fee392fb6e2d0ef932ad7d49da7a27dc62f42b7a
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2023-09-12 20:52:43 -0700

    Added SNS notification and switched to getting the variables from the os environment

--- call_api.py
+++ call_api.py
@@ -3,6 +3,8 @@ import sys
 import json
 from datetime import datetime
 import time
+import boto3
+import os
 
 RETRIES = 2
 DELAY = 60  # seconds
@@ -35,12 +37,13 @@ def check_data_period_end_date(data_period):
 
 
 def main():
-    if len(sys.argv) > 1:
-        site_id = sys.argv[1]
-        api_key = sys.argv[2]
-    else:
-        site_id = input("Please enter your SolarEdge Site ID: ").strip()
-        api_key = input("Please enter your SolarEdge API key: ").strip()
+    # Retrieve site_id and api_key from environment variables
+    site_id = os.environ.get('SITE_ID')
+    api_key = os.environ.get('API_KEY')
+
+    if not site_id or not api_key:
+        print("Error: $SITE_ID and $API_KEY each must be set.")
+        sys.exit(1)
 
     endpoints = {
         # "Site Details": BASE_URL.format(site_id, "details"),
@@ -72,4 +75,23 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    sns_client = boto3.client('sns')
+
+    topic_arn = os.environ.get('SNS_TOPIC_ARN')
+    if not topic_arn:
+        print("Error: $SNS_TOPIC_ARN must be set.")
+        sys.exit(1)
+
+    try:
+        main()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        # Send SNS notification
+        sns_client.publish(
+            TopicArn=topic_arn,
+            Message=f"Inverter script failed with error: {e}. Inverter may be down.",
+            Subject="Inverter Script Failure Alert"
+        )
+        sys.exit(1)
+
